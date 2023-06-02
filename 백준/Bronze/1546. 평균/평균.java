e554e15c836b09ffea2532f72b3b81fa1b15aeb7
LeeSeunghyuk <38344876+devmelonlee@users.noreply.github.com>
2023-06-02 09:07:58 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,26 +1,31 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n, m = 0;
-    float tmp, ret = 0.0f;
+    double ret = 0.0;
 
-    Scanner sc = new Scanner(System.in);
-    n = sc.nextInt();
+    n = Integer.parseInt(br.readLine());
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    
     int[] arr = new int[n];
     for(int i = 0; i < n; i++) {
-      arr[i] = sc.nextInt();
+      arr[i] = Integer.parseInt(st.nextToken());
     }
-    sc.close();
 
     Arrays.sort(arr);
     m = arr[arr.length-1];
 
     for(int i = 0; i < n; i++) {
-      tmp = ((arr[i] / (float) m) * 100);
-      ret += tmp;
+      ret += ((arr[i] / (double) m) * 100);
     }
+    
     ret /= n;
     System.out.println(ret);
   }
