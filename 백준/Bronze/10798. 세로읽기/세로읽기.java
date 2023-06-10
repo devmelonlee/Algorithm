947fececfcebde0508e1d154f66540984781e080
LeeSeunghyuk <38344876+devmelonlee@users.noreply.github.com>
2023-06-10 22:32:59 +0900

    [Bronze I] Title: 세로읽기, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+// 세로읽기
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuffer sb = new StringBuffer();
+
+    char[][] chArr = new char[5][15];
+    int maxLen = 0;
+    for (int i = 0; i < 5; i++) {
+      String str = br.readLine();
+      if (maxLen < str.length()) {
+        maxLen = str.length();
+      }
+      for (int j = 0; j < str.length(); j++) {
+        chArr[i][j] = str.charAt(j);
+      }
+    }
+
+    for (int j = 0; j < maxLen; j++) {
+      for (int i = 0; i < 5; i++) {
+        if (chArr[i][j] == '\0') continue;
+        sb.append(chArr[i][j]);
+      }
+    }
+    
+    System.out.println(sb);
+  }
+}
