bca30ec1d5ad5480931f0ce49d46460747f328c6
LeeSeunghyuk <38344876+devmelonlee@users.noreply.github.com>
2023-06-26 10:36:03 +0900

    [Silver II] Title: 유기농 배추, Time: 184 ms, Memory: 16192 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,9 +1,8 @@
 import java.io.*;
-
+import java.util.StringTokenizer;
+// 유기농 배추
 public class Main {
-  static int tc;
-  static int M;
-  static int N;
+  static int tc, N, M;
   static int[][] visited;
   static int[][] map;
   static final int[] dy = { -1, 0, +1, 0 };
@@ -15,51 +14,51 @@ public class Main {
     tc = Integer.parseInt(br.readLine());
 
     for (int i = 0; i < tc; i++) {
-      int cnt = 0;
       input();
+      int cnt = 0;
+
       for (int j = 0; j < N; j++) {
         for (int k = 0; k < M; k++) {
           if (map[j][k] == 1 && visited[j][k] == 0) {
             dfs(j, k);
             cnt++;
           }
-          
         }
       }
+
       sb.append("" + cnt + "\n");
     }
+
     System.out.println(sb);
   }
   
   public static void input() throws IOException {
-    String[] str = br.readLine().split(" ");
-    M = Integer.parseInt(str[0]);
-    N = Integer.parseInt(str[1]);
-    int K = Integer.parseInt(str[2]);
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    M = Integer.parseInt(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
     
-    map = new int[N+1][M+1];
-    visited = new int[N+1][M+1];
+    map = new int[N][M];
+    visited = new int[N][M];
     
     for (int i = 0; i < K; i++) {
-      String[] str2 = br.readLine().split(" ");
-      int x = Integer.parseInt(str2[0]);
-      int y = Integer.parseInt(str2[1]);
+      StringTokenizer st2 = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st2.nextToken());
+      int y = Integer.parseInt(st2.nextToken());
       map[y][x] = 1;
     }
   }
 
   static void dfs(int y, int x) {
-    if (y >= 0 && x >= 0 && y < N && x < M) {
-      if (map[y][x] == 1 && visited[y][x] == 0) {
-        visited[y][x] = 1;
-        for (int i = 0; i < 4; i++) {
-          dfs(y+dy[i], x+dx[i]);
-          dfs(y+dy[i], x+dx[i]);
-          dfs(y+dy[i], x+dx[i]);
-          dfs(y+dy[i], x+dx[i]);
-        }
+    if (y < 0 || x < 0 || y >= N || x >= M)
+      return;
+    if (map[y][x] == 1 && visited[y][x] == 0) {
+      visited[y][x] = 1;
+      for (int i = 0; i < 4; i++) {
+        dfs(y+dy[i], x+dx[i]);
       }
-    }
+    }    
+
   }
 
 }
