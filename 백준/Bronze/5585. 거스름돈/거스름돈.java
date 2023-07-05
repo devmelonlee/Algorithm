ac2497138aa8fb44954ff09c2ff2b2e414eca74b
LeeSeunghyuk <38344876+devmelonlee@users.noreply.github.com>
2023-07-05 23:38:43 +0900

    [Bronze II] Title: 거스름돈, Time: 124 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+    int n = 1000 - Integer.parseInt(br.readLine());
+    int[] arr = {500, 100, 50, 10, 5, 1};
+    int ans = 0;
+    for (int i : arr) {
+      if (n >= i) {
+        ans += n / i;
+        n %= i;
+      }
+    }
+    System.out.println(ans);
+  }
+}
