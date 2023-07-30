b0937369c946427581f2b566330b5d5d8ad3f5e6
LeeSeunghyuk <38344876+devmelonlee@users.noreply.github.com>
2023-07-31 00:35:59 +0900

    [Silver I] Title: 스타트링크, Time: 240 ms, Memory: 59372 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+// 스타트 링크
+public class Main {
+	static int f,s,g;
+	static int[] move;
+	static int[] dir = new int[2];
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		f = Integer.parseInt(st.nextToken());
+		s = Integer.parseInt(st.nextToken());
+		g = Integer.parseInt(st.nextToken());
+		dir[0] = Integer.parseInt(st.nextToken());
+		dir[1] = -Integer.parseInt(st.nextToken());
+		
+		move = new int[f + 1];
+		bfs(s);
+	}
+	
+	public static void bfs(int start) {
+		Queue<Integer> q = new LinkedList<>();
+		boolean[] check = new boolean[f + 1];
+		
+		q.add(start);
+		check[start] = true;
+		move[start] = 0;
+		
+		while(!q.isEmpty()) {
+			int pos = q.poll();
+			if(pos == g) { // 현재 위치 == 가야할곳 -> 출력
+				System.out.println(move[pos]);
+				return;
+			}
+			
+			for(int i = 0; i < 2; i++) {
+				int next = pos + dir[i];
+				if(next > f || next < 1) continue;
+				
+				if(!check[next]) {
+					check[next] = true;
+					q.add(next);
+					move[next]= move[pos]+1;
+				}
+			}
+		}
+		
+		System.out.println("use the stairs");
+	}
+}
