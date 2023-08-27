cf28df3762a1bb1e73b364f763e7e053ced8891e
LeeSeunghyuk <38344876+devmelonlee@users.noreply.github.com>
2023-08-27 23:00:48 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.26 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        
+        List<Integer> sc = new ArrayList();
+        for (int i = 0; i < score.length; i++) {
+            if (i < k-1){
+                sc.add(score[i]);
+                sc.sort(Collections.reverseOrder());
+                answer[i] = sc.get(sc.size()-1);
+            } else if (i >= k-1){
+                sc.add(score[i]);
+                sc.sort(Collections.reverseOrder());
+                answer[i] = sc.get(k-1);
+            }
+        }
+        return answer;
+    }
+}
